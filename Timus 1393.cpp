88962d17bc893504342885bea131ff76f7db1188
Eklas Mohin <120573102+BuggyLyfe@users.noreply.github.com>
2022-12-15 01:44:23 +0600

    Eniya

---
+++ Timus 1393.cpp
@@ -0,0 +1,20 @@
+/*
+*   Author       : Eklas Mohin
+*   Prolem Name  : Eniya
+*   Problem url  : https://acm.timus.ru/problem.aspx?space=1&num=1293
+*   Tags         : problem for beginners
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void __solve()
+{
+    int n{},a{},b{};
+    cin >> n >> a >> b;
+    cout<<2*n*a*b<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);cout.tie(0);cout.tie(0);
+    __solve();
+    return 0;
+}
