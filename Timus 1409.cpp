464c51d8374bb378a6b1a02895737a7692a45f0d
Eklas Mohin <120573102+BuggyLyfe@users.noreply.github.com>
2022-12-15 00:45:55 +0600

    Two Gangsters

---
+++ Timus 1409.cpp
@@ -0,0 +1,20 @@
+/*
+*   Author       : Eklas Mohin
+*   Prolem Name  : Two Gangsters
+*   Problem url  : https://acm.timus.ru/problem.aspx?space=1&num=1409
+*   Tags         : problem for beginners
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void __solve()
+{
+    int a{},b{};
+    cin >> a >> b;
+    cout<<b-1<<' '<<a-1<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);cout.tie(0);cout.tie(0);
+    __solve();
+    return 0;
+}
