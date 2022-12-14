c682e0fb077c3bc156424860f62a2223c7daf8b4
Eklas Mohin <120573102+BuggyLyfe@users.noreply.github.com>
2022-12-15 00:34:15 +0600

    A+B Problem

---
+++ Timus 1000.cpp
@@ -0,0 +1,20 @@
+/*
+*   Author       : Eklas Mohin
+*   Prolem Name  : A+B Problem
+*   Problem url  : https://acm.timus.ru/problem.aspx?space=1&num=1000
+*   Tags         : problem for beginners
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void __solve()
+{
+    int a{},b{};
+    cin >> a >> b;
+    cout<<a+b<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);cout.tie(0);cout.tie(0);
+    __solve();
+    return 0;
+}
